fd542cd1a2bcae21c40f794d28aa0ea4b95292ce
Danny C PC <dannyc52521@gmail.com>
2021-08-13 18:08:56 +1000

    Final update to drop-down menu

--- src/app.js
+++ src/app.js
@@ -8,8 +8,16 @@ const app = (function() {
   const components = document.querySelector("#components");
 
   navBar.addEventListener("mouseover", (e) => {
-    if (e.target === gaming) {
-      renderDropDownContent("gaming")
+    if(e.target === gaming) {
+      renderDropDownContent("gaming");
+      enableDropDown();
+    }
+    if(e.target === notebooks) {
+      renderDropDownContent("notebook");
+      enableDropDown();
+    }
+    if(e.target === components) {
+      renderDropDownContent("component");
       enableDropDown();
     }
   })
@@ -28,7 +36,7 @@ const app = (function() {
 
   const renderDropDownContent = function(content) {
     const gamingContent = `
-    <div class="gamingDropDown">
+    <main class="gamingDropDown">
     <div class="child">
       <h4>Apparel</h4>
       <ul class="apparel">
@@ -63,20 +71,165 @@ const app = (function() {
         <li>Accessories</li>
       </ul>
     </div>
-  </div>
+  </main>
   `;
-  const notebookContent = ``;
-  const componentsContent = ``;
-  switch (content) {
-    case "gaming":
-      dropDownContainer.innerHTML = gamingContent;
-      break;
-    case "notebook":
-      break;
-    case "components":
-      break;
-  } 
+  const notebookContent = `  
+  <main class="notebooks">
+      <div class="child2">
+        <div class="flex-row">
+          <img class="arrow" src="images/arrow.png" alt="">
+          <h4>Notebooks & Tablets</h4>
+        </div>
+        <div class="flex-row">
+          <img class="arrow" src="images/arrow.png" alt="">
+          <h4>Bags & Accessories</h4>
+        </div>
+        <div class="flex-row">
+          <img class="arrow" src="images/arrow.png" alt="">
+          <h4>Docking Stations</h4>
+        </div>
+        <div class="flex-row">
+          <img class="arrow" src="images/arrow.png" alt="">
+          <h4>Power Adaptors</h4>
+        </div>
+        <div class="flex-row">
+          <img class="arrow" src="images/arrow.png" alt="">
+          <h4>Extended Warranty</h4>
+        </div>
+      </div>
+      <div class="child2">
+        <h4>I need a device:</h4>
+        <ul>
+          <li>For Home</li>
+          <li>For Business</li>
+          <li>For Education</li>
+          <li>For Gaming</li>
+          <li>For CAD Workstation</li>
+          <li>That's Ulta Portable</li>
+          <li>That Converts Into a Table - 2 in 1</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Shop By Screensize</h4>
+        <ul>
+          <li>14" and under</li>
+          <li>Classic 15"6</li>
+          <li>17" and up</li>
+        </ul>
+        <h4>Shop by brand:</h4>
+        <img class="brand-images"src="images/asus.png" alt="">
+        <img class="brand-images" src="images/gigabyte.png" alt="">
+        <img class="brand-images" src="images/msi.png" alt="">
+        <img class="brand-images" src="images/razer.png" alt="">
+      </div>
+    </main>
+    `;
+    const componentContent = `   
+    <main class="component">
+    <div>
+      <h4>CPUs</h4>
+      <ul>
+        <li>AMD sTRX4</li>
+        <li>Intel LGA1200</li>
+        <li>AMD sWRX8</li>
+        <li>AMD AM4</li>
+        <li>Intel LGA2066</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Graphics Cards</h4>
+      <ul>
+        <li>AMD Workstation</li>
+        <li>External Display Adapters</li>
+        <li>AMD Radeon</li>
+        <li>Nvidia GeForce</li>
+        <li>Nvidia Workstation</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Hard Drives & SSDs</h4>
+      <ul>
+        <li>2.5" HDD</li>
+        <li>3.5" HDD</li>
+        <li>2.5" SSD</li>
+        <li>M.2 SSD</li>
+        <li>NAS Hard Drives</li>
+        <li>PCI Express SSD</li>
+        <li>Surveillance HDD</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Memory (RAM)</h4>
+      <ul>
+        <li>Desktop Memory</li>
+        <li>Notebook Memory</li>
+        <li>Server Memory</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Motherboards</h4>
+      <ul>
+        <li>AMD Socket sWRX8</li>
+        <li>Motherboard Accessories</li>
+        <li>AMD Socket AM4</li>
+        <li>AMD Socket TRX4</li>
+        <li>Intel Socket 1151-CL</li>
+        <li>Intel Socket 1200</li>
+        <li>Intel Socket 2011-3</li>
+        <li>Intel Socket 2066
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4>Optical Drives</h4>
+      <ul>
+        <li>Blu-Ray Drive</li>
+        <li>DVD Drive</li>
+        <li>External (USB)</li>
+      </ul>
+    </div>
+    <div>
+      <h4>Power Supplies</h4>
+      <ul>
+        <li>80Plus White</li>
+        <li>80Plus Bronze</li>
+        <li>80Plus Gold</li>
+        <li>80Plus Platinum</li>
+        <li>80Plus Titanium</li>
+        <li>Up to 600W</li>
+        <li>650W-800W</li>
+        <li><800W-1000W/li>
+        <li></li>
+      </ul>
+    </div>
+    <div>
+      <h4>Sound Cards</h4>
+      <ul>
+        <li>External (USB)</li>
+        <li>PCIe</li>
+      </ul>
+    </div>
+  </main>
+  `;
+
+    switch (content) {
+      case "gaming":
+        setContent(gamingContent);
+        break;
+      case "notebook":
+        setContent(notebookContent);
+        break;
+      case "component":
+        setContent(componentContent);
+        break;
+    } 
   }
+
+  const setContent = function(contentType) {
+    dropDownContainer.innerHTML = '';
+    dropDownContainer.innerHTML = contentType
+  }
+
 })();
 
 /*  
